56a6d18d3bffb91e1832c5f0cd772e9f539011ce
sarwoo <50518637+sarwoo@users.noreply.github.com>
2023-01-02 10:22:15 +0000

    layer dense class

--- p4.py
+++ p4.py
@@ -1,33 +1,44 @@
-# inputs = [1, 2, 3, 2.5]
+import numpy as np
+
+# inputs = [[1, 2, 3, 2.5],
+#           [2.0, 5.0, -1.0, 2.0],
+#           [-1.5, 2.7, 3.3, -0.8]]
+          
 # weights = [[0.2, 0.8, -0.5, 1],
 #            [0.5, -0.91, 0.26, -0.5],
 #            [-0.26, -0.27, 0.17, 0.87]]
+
 # biases = [2, 3, 0.5]
-# 
-# # Output of current layer
-# layer_outputs = []
-# # For each neuron
-# for neuron_weights, neuron_bias in zip(weights, biases):
-#     # Zeroed output of given neuron
-#     neuron_output = 0
-#     # For each input and weight to the neuron
-#     for n_input, weight in zip(inputs, neuron_weights):
-#         # Multiply this input by associated weight
-#         # and add to the neuron's output variable
-#         neuron_output += n_input*weight
-# # Add bias
-#     neuron_output += neuron_bias
-#     # Put neuron's result to the layer's output list
-#     layer_outputs.append(neuron_output)
-# print(layer_outputs)
 
-import numpy as np
+# weights2 = [[0.1, -0.14, 0.5],
+#             [-0.5, 0.12, -0.33],
+#             [-0.44, 0.73, -0.13]]
 
-inputs = [1, 2, 3, 2.5]
-weights = [[0.2, 0.8, -0.5, 1],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
-biases = [2, 3, 0.5]
+# biases2 = [-1, 2, -0.5]
 
-output = np.dot(weights, inputs) + biases
-print(output)
+# layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
+# layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
+
+# print(layer2_outputs)
+
+np.random.seed(0)
+
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons) -> None:
+        self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs) -> None:
+        self.outout = np.dot(inputs, self.weights) + self.biases
+
+layer1 = Layer_Dense(4, 5)
+layer2 = Layer_Dense(5, 2)
+
+layer1.forward(X)
+# print(layer1.outout)
+layer2.forward(layer1.outout)
+print(layer2.outout)
